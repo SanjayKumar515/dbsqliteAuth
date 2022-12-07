f52b40530d16d950a713b09b54b6b66c4aa4904e
Sanjay Kumar <78333127+SanjayKumar515@users.noreply.github.com>
2022-12-08 01:22:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,74 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import SplashScreen from './src/screens/SplashScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import MaterialBottomNavigation from './src/screens/MaterialBottomNavigation';
+import Toast from 'react-native-toast-message';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Stack = createNativeStackNavigator();
+
+const Auth = () => {
+  return (
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="RegisterScreen"
+        component={RegisterScreen}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isAuthenticated: 'false',
+    };
+  }
+
+  componentDidMount() {
+    let isAuth = AsyncStorage.getItem('isAuthenticated');
+    this.setState({isAuthenticated: isAuth});
+  }
+
+  render() {
+    return (
+      <>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="SplashScreen">
+            <Stack.Screen
+              name="SplashScreen"
+              component={SplashScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Auth"
+              component={Auth}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="MaterialBottomNavigation"
+              component={MaterialBottomNavigation}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <Toast />
+      </>
+    );
+  }
+}
